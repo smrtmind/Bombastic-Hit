9a35a18cd4af23a3f08e3104738a69701949d33b
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2023-06-15 21:48:11 +0300

    bug fix, fps counter minor fix

--- Assets/CodeBase/Service/GameManager.cs
+++ Assets/CodeBase/Service/GameManager.cs
@@ -16,13 +16,8 @@ namespace CodeBase.Service
         public static Action LevelPrepareAction = default;
         public static Action LevelReadyAction = default;
         public static Action LevelStartAction = default;
-        public static Action StartCollectPeriodAction = default;
-        public static Action RestartCollectPeriodAction = default;
-        public static Action StartWarPeriodAction = default;
-        //public static Action<LevelResult> LevelFinishAction = default;
 
         [Header("Variables")] private bool playerIsLoaded = false;
-        //private float startLevelTime = default;
 
         private void Start()
         {
@@ -31,42 +26,23 @@ namespace CodeBase.Service
 
         private void Awake()
         {
-            //playerStorageSO.LoadPlayer();
-
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
-
             DOTween.SetTweensCapacity(1500, 50);
         }
 
         private void OnEnable()
         {
             playerStorage.LoadPlayer();
-            //LevelStartAction += StartLevel;
-            LevelReadyAction += LevelReady;
-            //LevelFinishAction += LevelFinish;
-            PlayerLoadedAction += PlayerLoaded;
-            //GameStartAction += StartGame;
-        }
 
-        //private void Awake()
-        //{
-        //    playerStorageSO.LoadPlayer();
-        //    GameStartAction?.Invoke();
-        //}
+            LevelReadyAction += LevelReady;
+            PlayerLoadedAction += PlayerLoaded;
+        }
 
         private void OnDisable()
         {
-            //LevelStartAction -= StartLevel;
             PlayerLoadedAction -= PlayerLoaded;
             LevelReadyAction -= LevelReady;
-            //LevelFinishAction -= LevelFinish;
-            //GameStartAction -= StartGame;
         }
-
-        //private void StartLevel()
-        //{
-        //    GamePanelController.OnPointsChanged?.Invoke();
-        //}
 
         private void OnDestroy()
         {
